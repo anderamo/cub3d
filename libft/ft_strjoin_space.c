295420067cd01b6ef014897b5a6619d212a78d54
anderamo <82433445+anderamo@users.noreply.github.com>
2022-11-24 12:09:42 +0100

    Update ft_strjoin_space.c

--- libft/ft_strjoin_space.c
+++ libft/ft_strjoin_space.c
@@ -30,6 +30,7 @@ char	*ft_strjoin_space(char const *s1, char const *s2)
 	if (!array)
 		return (0);
 	ft_memmove(array, s1, s1_size);
+	array[s1_size] = ' ';
 	ft_memmove(array + s1_size + 1, s2, s2_size);
 	array[total_size + 1] = '\0';
 	free((char *)s1);
